c48961ee070d71cc6de8caebdb786996e77c2a06
TKspayair <tkworking2003@gmail.com>
2025-06-03 17:59:14 +0900

    deploy to heroku

--- memo.php
+++ memo.php
@@ -1,10 +1,29 @@
 <?php
-/*データベースの情報
+/*データベースの情報  for Heroku
 mysql://qjipgoolplpzyjpv:rprbtkbk27gbxm45@olxl65dqfuqr6s4y.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ov222yw3bxxxsmye
 
-User_name:qjipgoolplpzyjpv
-Password:rprbtkbk27gbxm45
-Host_name:olxl65dqfuqr6s4y.cbetxkdyhwsb.us-east-1.rds.amazonaws.com
-Database_name:ov222yw3bxxxsmye
+User_name:
+  qjipgoolplpzyjpv
+
+Password:
+  rprbtkbk27gbxm45
+
+Host_name:
+    olxl65dqfuqr6s4y.cbetxkdyhwsb.us-east-1.rds.amazonaws.com
+
+Database_name:
+  ov222yw3bxxxsmye
+
+Addons:         jawsdb-maria:kitefin
+Auto Cert Mgmt: false
+Dynos:          web: 1
+Git URL:        https://git.heroku.com/tako-laravel-nagoyameshi.git
+Owner:          tkworking2003@gmail.com
+Region:         us
+Repo Size:      144 KB
+Slug Size:      76 MB
+Stack:          heroku-24
+Web URL:        https://tako-laravel-nagoyameshi-cc8ba29fdc45.herokuapp.com/
 */
+
 ?>
