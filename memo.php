c162a2dbaabc9ae02dd7a9b90300507dceb4c44d
TKspayair <tkworking2003@gmail.com>
2025-06-03 15:00:01 +0900

    Herokuへのデプロイ準備

---
+++ memo.php
@@ -0,0 +1,10 @@
+<?php
+/*データベースの情報
+mysql://qjipgoolplpzyjpv:rprbtkbk27gbxm45@olxl65dqfuqr6s4y.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ov222yw3bxxxsmye
+
+User_name:qjipgoolplpzyjpv
+Password:rprbtkbk27gbxm45
+Host_name:olxl65dqfuqr6s4y.cbetxkdyhwsb.us-east-1.rds.amazonaws.com
+Database_name:ov222yw3bxxxsmye
+*/
+?>
