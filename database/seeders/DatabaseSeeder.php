016d32220c864d225f2766e1ab1562a8450ef7a5
TKspayair <tkworking2003@gmail.com>
2025-06-04 15:52:41 +0900

    DatabaseSeeder.phpno

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+      $this->call([
+            UserSeeder::class // ここにUserSeederを追加
+            // 他に実行したいシーダーがあればここに追加
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
-        // ]);
+        ]);
     }
 }
